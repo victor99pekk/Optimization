925b46caaeadac99aac4277d8353ebd5529920b6
Victor Pekkari <victor.pekkari@gmail.com>
2025-02-07 15:48:17 -0800

    safety

--- optimization2/hw3/hw3.py
+++ optimization2/hw3/hw3.py
@@ -34,20 +34,16 @@ y = np.concatenate((y_1, y_2), axis=0)
 lr = 1e-5
 
 N = 4000
-batch_size = 1
 
 
-def gradient(weights:np.ndarray[float], batch_size:int=batch_size) -> np.ndarray[float]:
+def gradient(weights:np.ndarray[float], batch_size:int=1) -> np.ndarray[float]:
     indices = np.random.choice(np.arange(0, 4000), size=batch_size, replace=True)
     grad = np.zeros_like(weights)
     for index in indices:
         x_multiplied_y = (-y[index]) * X[index]
         denominator = 1 + np.exp(-np.dot(weights, X[index]) * y[index])
         grad += (1 / batch_size) * x_multiplied_y * (1 - (1 / denominator))
-    return grad 
-
-def log_function(weights:np.ndarray[float]) -> float:
-    return np.log(function(weights))    
+    return grad   
 
 def classify(weights:np.ndarray[float]) -> float:
     test_data = pd.read_csv(test_csv)
@@ -75,12 +71,20 @@ def classify(weights:np.ndarray[float]) -> float:
             summation += 1
     return summation / 500
 
+def log_function(weights:np.ndarray[float]) -> float:
+    return np.log(function(weights))  
 
 def function(weights:np.ndarray[float]) -> float:
     #return (1 / N) * np.sum(np.log(1 + np.exp(-y[:N] * np.dot(X[:N], weights))))
     return (1 / N) * np.sum(np.log(1 + np.exp(-y[i] * np.dot(weights, X[i]))) for i in range(N))
 
-def sgd(save_fig:bool=False, nbr_iterations:int = 2000) -> None:
+def learning_rate(kind, t) -> float:
+    if kind == 'a':
+        return 1e-5
+    else:
+        return 1e-4 * np.sqrt(1/(1+t))
+
+def sgd(lr:str, save_fig:bool=False, nbr_iterations:int = 2000, batch_size=1) -> None:
     weights = np.zeros(X.shape[1])
     function_list = []
     iteration_list = []
@@ -88,17 +92,26 @@ def sgd(save_fig:bool=False, nbr_iterations:int = 2000) -> None:
     for i in range(nbr_iterations):
         iteration_list.append(i)
         function_list.append(log_function(weights))
-        weights -= lr * gradient(weights)
+        weights -= learning_rate(kind=lr, t=i) * gradient(weights, batch_size=batch_size)
+    
     if save_fig:
         plt.plot(iteration_list, function_list, label='Log-function value')
         plt.xlabel('#iterations')
         plt.ylabel('Log (function(w))')
         plt.title('SGD Optimization')
         plt.legend()
-        plt.savefig('sgd_optimization_plot1.png')
+        plt.savefig('sgd_optimization_plot2.png')
         plt.show()
     print(function_list[-1])
     return weights
 
-w = sgd(save_fig=True, nbr_iterations=2000)
+# 1a
+w = sgd(lr='a', save_fig=False, nbr_iterations=2000) 
+
+# 2a
+# w = sgd(lr='b', save_fig=False, nbr_iterations=2000) 
+
+# 3a
+# w = sgd(lr='b', save_fig=True, nbr_iterations=2000, batch_size=4000) 
+
 print(classify(w))
